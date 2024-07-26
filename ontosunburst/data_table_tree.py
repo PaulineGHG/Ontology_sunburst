b0300445c7f7e8c7a80eb0f6006655a38ed0cba1
phamongi <octavia.phg@gmail.com>
2024-07-26 16:18:35 +0200

    Make cut nested path method tests

--- ontosunburst/data_table_tree.py
+++ ontosunburst/data_table_tree.py
@@ -284,31 +284,7 @@ class DataTable:
                         c_count = self.count[c_i]
                         if p_count == c_count:
                             nested_paths.append(self.get_full_nested_path(c_i, [p_i]))
-            to_del = []
-            if mode == PATH_DEEPER:
-                for path in nested_paths:
-                    to_del += path[:-1]
-                    to_keep = path[-1]
-                    root_p = self.parents[path[0]]
-                    self.parents[to_keep] = root_p
-                    self.labels[to_keep] = '... ' + self.labels[to_keep]
-            elif mode == PATH_HIGHER:
-                for path in nested_paths:
-                    to_del += path[1:]
-                    to_keep = path[0]
-                    to_keep_c = [i for i in range(self.len) if self.parents[i] == self.ids[path[-1]]]
-                    for c_i in to_keep_c:
-                        self.parents[c_i] = self.ids[to_keep]
-                    self.labels[to_keep] += ' ...'
-            elif mode == PATH_BOUND:
-                for path in nested_paths:
-                    to_del += path[1:-1]
-                    to_keep_up = path[0]
-                    to_keep_do = path[-1]
-                    self.parents[to_keep_do] = self.ids[to_keep_up]
-                    self.labels[to_keep_up] += ' ...'
-                    self.labels[to_keep_do] = '... ' + self.labels[to_keep_do]
-            self.delete_value(to_del)
+            self.delete_nested_path(mode, nested_paths)
 
     def get_full_nested_path(self, p_i, n_path):
         n_path.append(p_i)
@@ -321,6 +297,33 @@ class DataTable:
             if p_count == c_count:
                 n_path = self.get_full_nested_path(c_i, n_path)
         return n_path
+
+    def delete_nested_path(self, mode, nested_paths):
+        to_del = []
+        if mode == PATH_DEEPER:
+            for path in nested_paths:
+                to_del += path[:-1]
+                to_keep = path[-1]
+                root_p = self.parents[path[0]]
+                self.parents[to_keep] = root_p
+                self.labels[to_keep] = '... ' + self.labels[to_keep]
+        elif mode == PATH_HIGHER:
+            for path in nested_paths:
+                to_del += path[1:]
+                to_keep = path[0]
+                to_keep_c = [i for i in range(self.len) if self.parents[i] == self.ids[path[-1]]]
+                for c_i in to_keep_c:
+                    self.parents[c_i] = self.ids[to_keep]
+                self.labels[to_keep] += ' ...'
+        elif mode == PATH_BOUND:
+            for path in nested_paths:
+                to_del += path[1:-1]
+                to_keep_up = path[0]
+                to_keep_do = path[-1]
+                self.parents[to_keep_do] = self.ids[to_keep_up]
+                self.labels[to_keep_up] += ' ...'
+                self.labels[to_keep_do] = '... ' + self.labels[to_keep_do]
+        self.delete_value(to_del)
 
     def delete_value(self, v_index: int or List[int]):
         data = self.get_data_dict()
